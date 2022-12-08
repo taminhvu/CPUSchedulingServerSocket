7a9b864f6d5e32e368470e5e8ead7bdc6735db20
Minh vu <taminhvu71@gamil.com>
2022-12-08 08:46:06 +0700

    update

--- src/main/java/entities/ddnn.java
+++ src/main/java/entities/ddnn.java
@@ -3,17 +3,22 @@ package entities;
 import com.google.common.graph.MutableValueGraph;
 import com.google.common.graph.ValueGraph;
 import com.google.common.graph.ValueGraphBuilder;
+import java.io.File;
 import pathfinding.dijkstra.DijkstraWithPriorityQueue;
 import pathfinding.dijkstra.Edge;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 @SuppressWarnings({"squid:S106", "PMD.SystemPrintln"})
 public class ddnn {
     public static Ddnode nodes=new Ddnode();
     static String str2="";
     ArrayList<Edge> edges = new ArrayList<Edge>();
+    HashMap<String, String> canh = new HashMap<String, String>();
 
     public ddnn(Ddnode nodes) {
         this.nodes=nodes;
@@ -53,11 +58,16 @@ public class ddnn {
                 String tam2= arrSplit[1].toString();
                 int tam3= Integer.parseInt(arrSplit[2]);
                 graph.putEdgeValue(tam1, tam2, tam3);
+                if(kiemtracanhtrung(tam1,tam2)==false){
+                    break;
+                }
+                canh.put(tam1, tam2);
                 edges.add(new Edge(Integer.parseInt(tam1), Integer.parseInt(tam2), tam3));
             }catch(Exception e){
                 return graph;
             }
         }
+
         return graph;
 
     }
@@ -69,4 +79,29 @@ public class ddnn {
         g.calculateShortestDistances(source);
         return g.printResult(dest);
     }
+    public boolean kiemtracanhtrung(String node1, String node2){
+        for(Map.Entry entry:canh.entrySet()){
+            if(Integer.parseInt(entry.getValue().toString())==Integer.parseInt(node1)){
+                if(Integer.parseInt(entry.getKey().toString())==Integer.parseInt(node1)){
+                    return false;
+                }
+                if(Integer.parseInt(entry.getKey().toString())==Integer.parseInt(node2)){
+                    //System.out.println("Chay");
+                    return false;
+                }
+
+            }
+            if(Integer.parseInt(entry.getValue().toString())==Integer.parseInt(node2)){
+                if(Integer.parseInt(entry.getKey().toString())==Integer.parseInt(node1)){
+                    return false;
+                }
+                if(Integer.parseInt(entry.getKey().toString())==Integer.parseInt(node2)){
+                    //System.out.println("Chay");
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
